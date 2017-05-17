2cdb774d7c80b91ed7de5db75f3b67cd444c2772
saksitaums <saksitth@scg.co.th>
2017-05-17 09:48:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,11 +1,16 @@
 <?php
-$access_token = 'XNhkwPW2XEiQ4ucjc0+IHLpHfMVY5UzXvQHJ5v8TwKqg0ME0EinfbyxIqZES7d5BAZ1oooj4uAfd+6W5TLhtR0Q2K2QGH4kP6gGE0PQbwtWgBnos/znVRFdp04e6Kywu9Aw0A/UuJ+3GE9Nso7VYrwdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
+ 
+$strAccessToken = 'XNhkwPW2XEiQ4ucjc0+IHLpHfMVY5UzXvQHJ5v8TwKqg0ME0EinfbyxIqZES7d5BAZ1oooj4uAfd+6W5TLhtR0Q2K2QGH4kP6gGE0PQbwtWgBnos/znVRFdp04e6Kywu9Aw0A/UuJ+3GE9Nso7VYrwdB04t89/1O/w1cDnyilFU=';
+ 
 $content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
@@ -26,29 +31,18 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
 }
-echo "OK";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
