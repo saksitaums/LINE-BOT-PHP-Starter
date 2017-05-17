5b77d9142e03449a5ef5671745efe2da56884203
saksitaums <saksitth@scg.co.th>
2017-05-17 16:10:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,8 +37,9 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = fuck ($ch);
-			curl_close($ch);
+			$result = curl_exec($ch);
+
+			curl_close($ch1);
 
 			echo $result . "\r\n";
 		}
