941c6b19bc93cdb12eeb8b7107dc3093769ac96b
saksitaums <saksitth@scg.co.th>
2017-05-17 09:53:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'สวัสดี',
-				'text' => $สวัสดี
+				'สวัสดี' => $สวัสดี
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
